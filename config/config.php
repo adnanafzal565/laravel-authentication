b461354186dbc3d2798697691fd1bee309f62be3
Adnan Afzal <adnanafzal565@gmail.com>
2024-11-02 01:47:21 +0500

    API URL fixed.

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
 
 return [
     "app_name" => "Laravel Authentication",
-    "api_url" => "http://localhost:8888/opensource/laravel-authentication/public/api",
+    "api_url" => url("/api"),
     "token_secret" => "token_secret1234567890"
 ];
