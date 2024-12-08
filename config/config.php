9bdef649d583696cf9e8e20cced81b8e44354d0c
Adnan Afzal <adnanafzal565@gmail.com>
2024-12-08 05:53:12 +0500

    Config file updated.

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
 
 return [
     "app_name" => "Laravel Authentication",
-    "api_url" => url("/api"),
+    "api_url" => "http://127.0.0.1:8000/api",
     "admin_token_secret" => "admin_token_secret1234567890"
 ];
